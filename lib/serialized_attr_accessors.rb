d45dc52c3559880136e72d73696a8f0060a12b07
praveen.kumar.sinha <praveen.kumar.sinha@gmail.com>
2013-04-17 16:20:20 +0530

    Adding functionality to have sattr_accessor for more than one serialized attributes

--- lib/serialized_attr_accessors.rb
+++ lib/serialized_attr_accessors.rb
@@ -2,40 +2,76 @@ module SerializedAttrAccessors
   def self.included(base)
     base.extend(ClassMethods)
 
-    def unserialized_options
-      @so_hash ||= (self.send(self.class.send(:get_serialized_attr)) || {})
+    def unserialized_options(serialized_attr)
+      @so_hash ||= {}
+      (@so_hash[serialized_attr] ||= (self.send(serialized_attr) || {}))
     end
 
-    def populate_serialized_options
-      self.send("#{self.class.send(:get_serialized_attr)}=", @so_hash) if @so_hash
+    def populate_serialized_attributes
+      self.class.serialized_attribute_list.each do |parent_attr_key, value|
+        self.send("#{parent_attr_key.to_s}=", @so_hash[parent_attr_key]) if @so_hash and @so_hash[parent_attr_key]
+      end
     end
 
-    base.send(:before_validation, :populate_serialized_options)
+    base.send(:before_validation, :populate_serialized_attributes)
   end
 
   module ClassMethods
-    #Sets serialized attribute, Default serialized attribute name is "serialized_options" until and unless set manually
-    def set_serialized_attribute(attribute_name)
-      @ser_attr = attribute_name
+    #Stores list of attributes serialized
+    def serialized_attribute_list
+      @parent_attribute_list ||= {:serialized_options => []}
     end
 
-    def get_serialized_attr
-      @ser_attr ||= "serialized_options"
+    #Gets serialized attribute currenly in use
+    def current_serialized_attr
+      @curr_ser_attr ||= serialized_attribute_list.keys.first
     end
 
     #Generates getter and setter method with field_name and default_value (if provided else nil)
     #Example:
     # sattr_accessor :second_name, "kumar"
     # sattr_accessor :address
-    def sattr_accessor(filed_name, default_value = nil)
+    def sattr_accessor(field_name, default_value = nil)
+      field_name = field_name.to_sym unless field_name.is_a?(Symbol)
 
-      define_method filed_name do
-        (unserialized_options[filed_name.to_sym] || default_value)
+      serialized_attribute_list[current_serialized_attr] ||= []
+      serialized_attribute_list[current_serialized_attr] << field_name
+
+      #If attributes are not serialized then here is serialization done
+      self.serialize(current_serialized_attr) unless self.serialized_attributes.keys.include?(current_serialized_attr.to_s)
+
+      #Defining method to fetch serialzed parent attribute (gives last found)
+      define_method :fetch_parent_attribute do |filed_name|
+        parent_attr = nil
+        self.class.serialized_attribute_list.each do |attr_key, fields_val|
+          parent_attr = attr_key if fields_val.include?(filed_name)
+        end
+        raise "UnableToFindParentAttribute" if parent_attr.nil?
+        parent_attr
       end
 
-      define_method "#{filed_name}=" do |field_value|
-        unserialized_options.merge!(filed_name.to_sym => field_value)
+      define_method field_name do
+        (unserialized_options(fetch_parent_attribute(field_name))[field_name] || default_value)
+      end
+
+      define_method "#{field_name.to_s}=" do |field_value|
+        unserialized_options(fetch_parent_attribute(field_name)).merge!(field_name => field_value)
         field_value
+      end
+    end
+
+    #for_serialized_field :workspaces do
+    #  sattr_accessor(:wfield_one, "Some String One")
+    #  sattr_accessor(:wfield_two, {})
+    #  sattr_accessor(:wfield_three, [])
+    #end
+    def for_serialized_field(fieldname)
+      if block_given?
+        @curr_ser_attr = fieldname
+        yield
+        @curr_ser_attr = serialized_attribute_list.keys.first
+      else
+        raise "ExpectedBlockWithAttributes"
       end
     end
 
